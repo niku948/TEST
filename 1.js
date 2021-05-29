51ebd0eb0d33e70d1a0a49d2061ce746a2734617
niku948 <84850740+niku948@users.noreply.github.com>
2021-05-29 09:11:13 +0530

    Add files via upload
    Question 1

---
+++ 1.js
@@ -0,0 +1,10 @@
+var arr = [8,5,4,6,2,1,7,9];
+var even;
+
+document.writeln("even numbers:");
+for(var i = 0 ; i<= arr.length; i++){
+    if(arr[i]%2 == 0){
+        document.writeln(arr[i]);
+    }
+
+}
